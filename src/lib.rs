c402293e475846ed8ed868e13ab27bcdfecd29d9
Roberto De Ioris <roberto.deioris@gmail.com>
2018-09-12 17:42:16 +0200

    unixterm fixes

--- src/lib.rs
+++ src/lib.rs
@@ -35,28 +35,49 @@ mod tests {
     #[test]
     fn address_bus_io_u8_u8() {
         let mut bus : TestAddressBusIO<u8, u8> = TestAddressBusIO::default();
-        bus.write(0xff, 0);
+        bus.write(0xff, 0xff);
         assert_eq!(bus.read(0xff), 0);
     }
 
     #[test]
     fn address_bus_io_u16_u8() {
         let mut bus : TestAddressBusIO<u16, u8> = TestAddressBusIO::default();
-        bus.write(0xff, 0);
+        bus.write(0xff, 0xff);
         assert_eq!(bus.read(0xffff), 0);
     }
 
     #[test]
     fn address_bus_io_u32_u8() {
         let mut bus : TestAddressBusIO<u32, u8> = TestAddressBusIO::default();
-        bus.write(0xffffffff, 0);
+        bus.write(0xffffffff, 0xff);
         assert_eq!(bus.read(0xffffffff), 0);
     }
 
     #[test]
     fn address_bus_io_u64_u8() {
         let mut bus : TestAddressBusIO<u64, u8> = TestAddressBusIO::default();
-        bus.write(0xffffffffffffffff, 0);
+        bus.write(0xffffffffffffffff, 0xff);
         assert_eq!(bus.read(0xffffffffffffffff), 0);
+    }
+
+    #[test]
+    fn address_bus_io_u8_u16() {
+        let mut bus : TestAddressBusIO<u8, u16> = TestAddressBusIO::default();
+        bus.write(0xff, 0xffff);
+        assert_eq!(bus.read(0xff), 0);
+    }
+
+    #[test]
+    fn address_bus_io_u16_u32() {
+        let mut bus : TestAddressBusIO<u16, u32> = TestAddressBusIO::default();
+        bus.write(0xffff, 0xaabbccdd);
+        assert_eq!(bus.read(0xabcd), 0);
+    }
+
+    #[test]
+    fn address_bus_io_u64_u32() {
+        let mut bus : TestAddressBusIO<u64, u32> = TestAddressBusIO::default();
+        bus.write(0xffffffffaabbccdd, 0xaabbccdd);
+        assert_eq!(bus.read(0xaabbccddffaaffbb), 0);
     }
 }
--- src/unixterm/mod.rs
+++ src/unixterm/mod.rs
@@ -1,16 +1,40 @@
+use std::io::{stdin, stdout, stderr, Stdin, Stdout, Stderr};
+use std::io::Write;
+use std::io::Read;
+
 use AddressBusIO;
 
-pub struct UnixTerm {}
+pub struct UnixTerm {
+    stdin: Stdin, 
+    stdout: Stdout, 
+    stderr: Stderr,
+
+    last_stdout: u8,
+    last_stderr: u8,
+}
 
 impl UnixTerm {
     pub fn new() -> UnixTerm {
-        UnixTerm {}
+        UnixTerm {stdin: stdin(), stdout: stdout(), stderr: stderr(), last_stdout: 0, last_stderr: 0}
     }
 }
 
 impl AddressBusIO<u8, u8> for UnixTerm {
     fn read(&mut self, address: u8) -> u8 {
-        return 0;
+        match address {
+            0x01 => self.last_stdout, 
+            0x02 => self.last_stderr, 
+            _ => 0,
+        }
+    }
+
+    fn write(&mut self, address: u8, value: u8) {
+        let buffer = [value; 1];
+        match address {
+            0x01 => { self.stdout.write(&buffer).unwrap(); self.last_stdout = value;},
+            0x02 => { self.stderr.write(&buffer).unwrap(); self.last_stderr = value;},
+            _ => {},
+        }
     }
 }
 
@@ -22,8 +46,20 @@ impl AddressBusIO<u16, u8> for UnixTerm {
 
 #[cfg(test)]
 mod tests {
+    use unixterm::UnixTerm;
+    use AddressBusIO;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn write_stdout() {
+        let mut term  = UnixTerm::new();
+        term.write(0x01 as u8, 17 as u8);
+        assert_eq!(term.read(0x01 as u8), 17 as u8);
+    }
+
+    #[test]
+    fn write_stderr() {
+        let mut term  = UnixTerm::new();
+        term.write(0x02 as u8, 22 as u8);
+        assert_eq!(term.read(0x02 as u8), 22 as u8);
     }
 }
