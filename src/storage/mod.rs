3276367ade9ecc4b5efd9e5e70144c712003d8ee
Roberto De Ioris <roberto.deioris@gmail.com>
2018-11-05 15:01:11 +0100

    fixed storage seek

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -11,37 +11,49 @@ use {Address, AddressBusBlockIO, As};
 pub struct BlockDevice {
     file: File,
     pub block_size: usize,
+    max_size: u64,
 }
 
 impl BlockDevice {
-    pub fn new(file: File, block_size: usize) -> BlockDevice {
-        BlockDevice { file, block_size }
+    pub fn new(mut file: File, block_size: usize) -> BlockDevice {
+        let max_size = file.seek(SeekFrom::End(0)).unwrap();
+        BlockDevice { file, block_size, max_size }
     }
 
     pub fn from_filename<P: AsRef<Path>>(filename: P, block_size: usize) -> BlockDevice {
-        let file = OpenOptions::new()
+        let mut file = OpenOptions::new()
             .read(true)
             .write(true)
             .create(true)
-            .open(filename);
+            .open(filename).unwrap();
+        let max_size = file.seek(SeekFrom::End(0)).unwrap();
         BlockDevice {
-            file: file.unwrap(),
+            file: file,
             block_size,
+            max_size,
         }
     }
 }
 
 impl<T: Address + As<usize>> AddressBusBlockIO<T, u8> for BlockDevice {
     fn read(&mut self, address: T, buffer: &mut [u8]) {
+        let offset = (address.as_() * self.block_size) as u64;
+        if offset + self.block_size as u64 >= self.max_size {
+            return;
+        }
         self.file
-            .seek(SeekFrom::Start((address.as_() * self.block_size) as u64))
+            .seek(SeekFrom::Start(offset))
             .unwrap();
         self.file.read(buffer).unwrap();
     }
 
     fn write(&mut self, address: T, buffer: &[u8]) {
+        let offset = (address.as_() * self.block_size) as u64;
+        if offset + self.block_size as u64 >= self.max_size {
+            return;
+        }
         self.file
-            .seek(SeekFrom::Start((address.as_() * self.block_size) as u64))
+            .seek(SeekFrom::Start(offset))
             .unwrap();
         self.file.write(buffer).unwrap();
         self.file.sync_all().unwrap();
